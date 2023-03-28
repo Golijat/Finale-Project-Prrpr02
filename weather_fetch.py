01bfc2491a265be794856726f3dbab3a7e418bb1
Golijat <l.casserlund@gmail.com>
2023-03-28 14:31:56 +0200

    Updated the weather fetch file

--- weather_fetch.py
+++ weather_fetch.py
@@ -9,3 +9,15 @@ timeout = 10, headers={"User-Agent":"End-Projekt/0.0.1 github.com/Golijat/Finale
 Arvika = Arvika.json()
 arvika_temp = Arvika['properties']['timeseries'][0]["data"]["instant"]["details"]["air_temperature"]
 print(arvika_temp)
+
+class Fetch:
+    def __init__(self, lat, lon):
+        self.cur_temp = 0
+        self.cur_symbol = "NULL"
+        self.next_six_temp = 0
+        self.next_six_symbol = "NULL"
+        self.lat = lat
+        self.lon = lon
+    
+    def set_cords(self):
+        
