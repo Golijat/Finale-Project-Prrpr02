a8d0b506955c70025e4d246e8b423276b556802a
Golijat <l.casserlund@gmail.com>
2023-03-28 08:41:30 +0200

    Added a markdown class diagram.

---
+++ Class-Diagram.md
@@ -0,0 +1,34 @@
+|Class Name|Nu Ge Do Dig|
+|---|---|
+|Something| Else something |
+|Den|
+
+
+|Weather Fetch|
+|---|
+|***Attributes***|
+|current_weather_temp 
+current_weather_symbol_code
+next_six_hours_temp
+next_six_hours_symbol_code|
+|***Methods***|
+|fetchWeather|
+
+|Weather Get|
+|---|
+|***Attributes***|
+|current_weather_temp 
+current_weather_symbol_code
+next_six_hours_temp
+next_six_hours_symbol_code|
+|***Methods***|
+|get_current_weather_temp
+get_current_weather_symbol_code
+get_next_six_hours_temp
+get_next_six_hours_symbol_code|
+
+Använd fetch för att hämta väder information från yr.
+Använd tkinter för GUI där man kan visa informaitonen.
+Hämta väder informationen med en klass, ha sen en annan
+klass som ärver väder informationen och sen lägger ut
+på GUIen.
--- test.py
+++ test.py
@@ -2,3 +2,22 @@ print("Hello World!")
 
 for i in range(0,10):
     print("Echo")
+
+import json
+from bs4 import BeautifulSoup
+import requests
+
+Arvika = requests.get("https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=59.516667&lon=13.8", timeout = 10, headers={"User-Agent":"End-Projekt/0.0.1 github.com/Golijat/Finale-Project-Prrpr02"})
+print(Arvika)
+Arvika = Arvika.json()
+# print(Arvika)
+arvika_temp = Arvika['properties']['timeseries'][0]["data"]["instant"]["details"]["air_temperature"]
+print(arvika_temp)
+
+# # currenthour_Arvika = requests.get("https://www.yr.no/api/v0/\
+# # locations/2-2725123/forecast/currenthour", timeout = 10)
+# currenthour = currenthour_Arvika.json()
+# arvika_temp = currenthour['temperature']['value']
+# arvika_weather = currenthour['symbolCode']["next1Hour"]
+# print(f"The temperature right now in Arvika is {arvika_temp}\
+#  degrees Celcius and the weather is {arvika_weather}.")
